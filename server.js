4186bd4dc92cd87554d63267cb8dd24d339d3880
Jelena Grimshaw <muchstudio@hotmail.com>
2019-05-22 23:29:03 +0000

    switch to html, delete views

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(morgan('dev'));
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
-app.set("view engine", "ejs");
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
   src: __dirname + "/styles",
@@ -37,11 +37,6 @@ app.use(express.static("public"));
 
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
-
-// Home page
-app.get("/", (req, res) => {
-  res.render("index");
-});
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
