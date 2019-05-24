99677694ca191e0d2a38783f0781526f183917ef
Christian <c_pfeiffer325@yahoo.ca>
2019-05-24 23:18:51 +0000

    menu rendering from database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,19 @@
 // build the menu items by pulling data from db in compiling individual elements for rendering on the webpage
 function createMenuItem(menuData) {
   const picture = menuData.picture;
-  const $menu_item = $("<article>");
-
-  const $picture = $(`<img src=${picture}>`);
-  const $name = $("<h4>").text(menuData.name);
+  const $menu_item = $("<article>").addClass("thumbnail");
+  const $menu_item_text = $("<div>").addClass("menu-item-text");
+  const $price_and_add = $("<div>").addClass("price-and-add");
+    
+  const $picture = $(`<div class="menu-item-image">`).attr("style", `background: url('${picture}') no-repeat center / cover`);
+  const $name = $("<h4>").text(menuData.name).addClass("item-title");
   const $description = $("<p>").text(menuData.description).addClass("text");
-  const $price = $(`<h4 class="price">${menuData.price}</h4>`);
+  const $price = $(`<h4 class="price">$${menuData.price}</h4>`);
+  const $submit = $(`<button type="submit" class="btn btn-link add-item">Add to order</button>`);
 
-  $menu_item.append($picture).append($name).append($description).append($price);
+  $price_and_add.append($price).append($submit);
+  $menu_item_text.append($name).append($description).append($price_and_add);
+  $menu_item.append($picture).append($menu_item_text);
   return $menu_item;
 }
 
