092b86f93505975bc876c2d3244fba4bc266922c
Christian <c_pfeiffer325@yahoo.ca>
2019-05-24 19:10:55 +0000

    update route from user to menu

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
+const usersRoutes = require("./routes/menu");
 
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
