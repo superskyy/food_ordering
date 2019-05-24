718ea7b4c6fe234853219990abf52d39cdf786e0
Jelena Grimshaw <muchstudio@hotmail.com>
2019-05-24 00:53:13 +0000

    app.js functions, post request to /orders

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,74 @@ $(() => {
     }
   });;
 });
+
+// get the phone number from the input // POST to /order
+$('.phone-form').on('submit', function(){
+  $.ajax({
+    method: "POST",
+    url: "/order",
+    data: $(this).serialize(),
+    success: function () {
+      $('.phone-form').reset();
+      $("#thanks").text("Thank you for your order!").show()
+    }
+  })
+});
+
+function getPhoneNumber (){
+  return Number($('input.phone').val());
+};
+
+
+
+// create the list item
+function createOrder (title){
+
+    let $order = `
+    <ul class="order-summary">
+     <li>
+    ${title} ${price}
+    </li>
+    </ul> `
+
+    return $order;
+  }
+
+
+// get the title of the menu item after click on Add Order button
+
+const title = $('button.add-item').on('click', function(){
+  console.log(findItemTitle());
+  $('ul').append(findItemTitle());
+});
+
+function findItemTitle(){
+ return ($('#item-title').text());
+};
+
+// get the price form the menu item
+const price = $('button.add-item').on('click', function(){
+  console.log(findItemPrice());
+  $('ul').append(findItemPrice());
+});
+
+function findItemPrice(){
+ return ($('.price').text());
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
