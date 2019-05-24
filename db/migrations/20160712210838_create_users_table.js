c67105be94c90a235a82dfa811e75292247b4d40
Christian <c_pfeiffer325@yahoo.ca>
2019-05-24 01:03:31 +0000

    modify migrate and seed file

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,4 +1,17 @@
 exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function (table) {
+    table.increments();
+    table.string('name');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
+
+
+/*
+exports.up = function(knex, Promise) {
   return knex.schema
   .createTable('customer', function (table) {
     table.increments();
@@ -7,44 +20,27 @@ exports.up = function(knex, Promise) {
   })
   .createTable('cart', function (table) {
     table.increments();
-    table.integer('customer_id').references('customer(id)');
+    table.integer('customer_id');
+    table.foreign('customer_id').references('customer.id');
   })
   .createTable('menu', function (table) {
     table.increments();
     table.string('name');
+    table.string('picture');
+    table.string('description');
+    table.integer('price');
   })
   .createTable('cart_items', function (table) {
     table.increments();
-    table.string('cart_items');
+    table.integer('cart_id');
+    table.foreign('cart_id').references('cart.id');
+    table.integer('menu_id');
+    table.foreign('menu_id').references('menu.id');
+    table.integer('quantity');
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('cart_items','menu','cart','customer','users');
+  return knex.schema.dropTable('users','cart_items','menu','cart','customer');
 };
-
-/*
-CREATE TABLE customer (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(50) NOT NULL,
-  phone_number VARCHAR(15)
-);
-
-CREATE TABLE cart (
-  id SERIAL PRIMARY KEY NOT NULL,
-  customer_id INTEGER REFERENCES customer(id) ON DELETE CASCADE
-);
-
-CREATE TABLE menu (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(50) NOT NULL,
-  price MONEY
-);
-
-CREATE TABLE cart_items (
-  id SERIAL PRIMARY KEY NOT NULL,
-  cart_id INTEGER REFERENCES cart(id) ON DELETE CASCADE,
-  menu_id INTEGER REFERENCES menu(id) ON DELETE CASCADE,
-  price MONEY
-);
 */
